423c98ac186e369687083b58bea21c01d920e412
unknown <amipatel46226@gmail.com>
2023-11-08 16:59:29 -0700

    copyrigt symbol added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,6 +3,6 @@ import React from "react";
 
 
 function Footer(){
-    return <p className="footer">copyright {new Date().getFullYear()}</p>
+    return <p className="footer">Copyright © {new Date().getFullYear()}</p>
 }
 export default Footer;
