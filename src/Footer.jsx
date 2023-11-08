b3c1fcb084f261ce918596e72067e898e0cbf222
unknown <amipatel46226@gmail.com>
2023-11-08 16:18:16 -0700

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+
+function Footer(){
+    return <p className="footer">copyright {new Date().getFullYear()}</p>
+}
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return(
+        <div className="notes">
+        <p>This in note title</p>
+        <p>This in note content</p>
+        </div>
+    )
+   
+}
+export default Note;
